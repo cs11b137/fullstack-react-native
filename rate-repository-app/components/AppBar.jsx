d7ebbeeb6fe0d64fd4c41edf464ec3eddcc12e49
wuwj <wuwj@allinpay.com>
2021-03-23 18:14:51 +0800

    update

---
+++ rate-repository-app/components/AppBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import Constants from 'expo-constants';
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: Constants.statusBarHeight
+    }
+});
+
+const AppBar = () => {
+    return <View style={styles.container}><Text>appbar</Text></View>;
+};
+
+export default AppBar;
